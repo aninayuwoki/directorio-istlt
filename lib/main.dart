4aedf795a31ee32573934414d71480e06ee77d0e
anina <anina@localhost>
2026-07-20 20:04:42 -0500

    agregamos iconos a la lista que hicimos

--- lib/main.dart
+++ lib/main.dart
@@ -9,10 +9,43 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+        appBar: AppBar(
+          title: const Text('Directorio IST La Troncal'),
+          backgroundColor: Colors.blue,
+          foregroundColor: Colors.yellow,
+        ),
+        body: ListView(
+          children: <Widget>[
+            ListTile(
+              leading: Icon(Icons.person, color: Colors.blue),
+              title: Text('Ing. Israel Zurita'),
+              subtitle: Text('Desarrollo de aplicaciones web'),
+              trailing: Icon(
+                Icons.star,
+                color: const Color.fromARGB(255, 194, 192, 170),
+              ),
+            ),
+            ListTile(
+              leading: Icon(Icons.person, color: Colors.blue),
+              title: Text('Ing. Angel Novillo'),
+              subtitle: Text('Desarrollo de aplicaciones web'),
+              trailing: Icon(
+                Icons.star,
+                color: const Color.fromARGB(255, 180, 175, 138),
+              ),
+            ),
+            ListTile(
+              leading: Icon(Icons.person, color: Colors.blue),
+              title: Text('Ing. Cecilia Naula'),
+              subtitle: Text('Desarrollo de aplicaciones web'),
+              trailing: Icon(
+                Icons.star,
+                color: const Color.fromARGB(255, 218, 212, 165),
+              ),
+            ),
+          ],
         ),
       ),
     );
